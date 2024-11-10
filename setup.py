1504b0d3a77aa542f04914c925177d940130d07d
lucyvogel <lucy.vogel.29@gmail.com>
2024-11-10 21:15:21 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='math_quiz',
+    version="1.0",
+
+    url='//github.com/lucyvogel/dsss_homework_2.git',
+    author='Lucy Vogel',
+    author_email='lucy.vogel.29@gmail.com',
+
+    py_modules=['math_quiz'],
+)
